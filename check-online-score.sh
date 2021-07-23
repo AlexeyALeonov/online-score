a71d3758649287bc886425646ccf1b0ca95f637b
Aleksei A. Leonov <aleksei.leonov@bookedby.com>
2021-07-23 16:55:38 +0300

    added nice view for bash version

--- check-online-score.sh
+++ check-online-score.sh
@@ -1,5 +1,4 @@
 for item in `curl -sL http://localhost:14002/api/sno | jq '.satellites[].id' -r`; do
-    windows=`curl -s http://localhost:14002/api/sno/satellite/$item | \
-    jq '.auditHistory.windows[] | select(.totalCount != .onlineCount)' -c`
-    [ "${windows:-}" ] && echo ${item} && echo $windows | jq '.'
+    curl -s http://localhost:14002/api/sno/satellite/$item | \
+    jq '{id: .id, auditHistory: [.auditHistory.windows[] | select(.totalCount != .onlineCount)]}'
 done
